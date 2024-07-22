6fb30aba4da2442a98d52f78c1fa2c1845e257af
Kirill Tulyakov <omgnii007@gmail.com>
2024-07-23 02:54:18 +0300

    Made corrections: Since all my binding implementations were through a constructor, I changed the binding type in Duck5 to setter and binding in Egg6 to field. Testing ok.

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,12 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle7;
-
     @Autowired
-    public Egg6(Needle7 needle7) {
-        this.needle7 = needle7;
-    }
+    private Needle7 needle7;
 
     @Override
     public String toString() {
